db9e40f71e26f4226ffc1379e59dec65757fe7f3
ttdo19 <thuytrang19072001@gmail.com>
2023-08-15 11:30:02 -0500

    Add the clear and eraser button

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+let hasBorder = 1;
+let eraserOn = 0; 
+
 const container = document.getElementById("container"); 
 const slider = document.getElementById("myRange")
 const numDiv = document.querySelector("#num-divs")
-const toggleGrid = document.querySelector("#toggle-grid"); 
+const toggleGridBtn = document.querySelector("#toggle-grid"); 
+const clearBtn = document.querySelector("#clear"); 
+const eraserBtn = document.querySelector("#eraser");
 
-toggleGrid.addEventListener('click', toggleBorder); 
-
-let hasBorder = 1; 
+toggleGridBtn.addEventListener('click', toggleBorder); 
+clearBtn.addEventListener('click', clearColor); 
+eraserBtn.addEventListener('click', toogleEraser); 
 
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows); 
@@ -17,11 +22,16 @@ function makeRows(rows, cols) {
         let cell = document.createElement("div"); 
         // cell.innerText = (i+1); 
         container.appendChild(cell).className = cellClassName; 
+        cell.addEventListener('mouseenter', addHoverEffect); 
     }
 }
 
+//by default 
+makeRows(50, 50); 
+numDiv.innerHTML = "Grid size: 50x50"; 
+
 slider.oninput = function() {
-    numDiv.innerHTML = this.value + 'x' + this.value + " Grid"; 
+    numDiv.innerHTML = "Grid size: " + this.value + 'x' + this.value; 
     deleteGrid(); 
     makeRows(this.value, this.value); 
 }
@@ -37,4 +47,22 @@ function toggleBorder() {
     const cells = document.querySelectorAll(".grid-item"); 
     cells.forEach((cell)=> cell.classList.toggle("with-border")); 
     hasBorder = !hasBorder; 
+    toggleGridBtn.classList.toggle("clicked"); 
+}
+
+function addHoverEffect(e) {
+    if (eraserOn) e.target.style.backgroundColor = 'white'; 
+    else e.target.style.backgroundColor = 'black'; 
+}
+
+function clearColor() {
+    const cells = document.querySelectorAll(".grid-item");
+    cells.forEach((cell)=> cell.style.backgroundColor = 'white'); 
+    clearBtn.classList.add("clicked"); 
+    setTimeout(() => clearBtn.classList.remove("clicked"), 400); 
+}
+
+function toogleEraser() {
+    eraserOn = !eraserOn
+    eraserBtn.classList.toggle("clicked"); 
 }
